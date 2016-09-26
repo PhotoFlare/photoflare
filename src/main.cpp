fee3c88c5c9d8cf8e2c5defd703e15dc19d7cca5
Dylan <info@photofiltre-lx.org>
2016-09-26 16:33:51 +0100

    Build 70
    Fixed single application code.

--- src/main.cpp
+++ src/main.cpp
@@ -9,46 +9,51 @@
 #include <QApplication>
 #include <QFileInfo>
 #include <QTranslator>
+#include <QtSingleApplication>
 #include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QtSingleApplication app("PhotoFiltre LX Studio Demo", argc, argv);
 
-    if(!app.isRunning())
-    {
-        app.setApplicationName(QObject::tr("PhotoFiltre LX Studio Demo"));
-        app.setApplicationVersion(QObject::tr("0.9 demo version (Build 61)"));
-        app.setOrganizationDomain(QObject::tr("photofiltre-lx.org"));
-        app.setOrganizationName(QObject::tr("photofiltre-lx"));
+        if(!app.isRunning())
+        {
+            app.setApplicationName(QObject::tr("PhotoFiltre LX Studio Demo"));
+            app.setApplicationVersion(QObject::tr("0.9 demo version (Build 70)"));
+            app.setOrganizationDomain(QObject::tr("photofiltre-lx.org"));
+            app.setOrganizationName(QObject::tr("photofiltre-lx"));
 
-    QTranslator translator;
-    if(translator.load("languages/fr.qm", app.applicationDirPath()))
-        qDebug() << "Loaded translation";
-    else
-        qDebug() << "Translation not loaded";
-    app.installTranslator(&translator);
+            QTranslator translator;
+            if(translator.load("languages/fr.qm", app.applicationDirPath()))
+                qDebug() << "Loaded translation";
+            else
+                qDebug() << "Translation not loaded";
+            app.installTranslator(&translator);
 
-        if(argc > 1) {
-            for (int i = 1; i < argc; ++i) {
-                    w.handleMessage(QString(argv[i]));
+
+            MainWindow w;
+            w.show();
+
+            if(argc > 1) {
+                for (int i = 1; i < argc; ++i) {
+                        w.handleMessage(QString(argv[i]));
+                    }
                 }
-            }
 
-        app.setActivationWindow(&w, true);
+            app.setActivationWindow(&w, true);
 
-        QObject::connect(&app, SIGNAL(messageReceived(QString)), &w, SLOT(handleMessage(QString)));
+            QObject::connect(&app, SIGNAL(messageReceived(QString)), &w, SLOT(handleMessage(QString)));
 
-        return app.exec();
+            return app.exec();
 
-    } else
-    {
-        if(argc > 1) {
-            for (int i = 1; i < argc; ++i) {
-                    app.sendMessage(QString(argv[i]));
+        } else
+        {
+            if(argc > 1) {
+                for (int i = 1; i < argc; ++i) {
+                        app.sendMessage(QString(argv[i]));
+                    }
                 }
-            }
-    }
+        }
 
-    return 0;
+        return 0;
 }
