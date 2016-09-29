b274720b437a9155b86f0ccd5ddee96da0ad8993
Dylan <info@photofiltre-lx.org>
2016-09-29 09:37:41 +0100

    Build 93
    Add new brush set for Advanced Brush.

--- src/main.cpp
+++ src/main.cpp
@@ -21,7 +21,7 @@ int main(int argc, char *argv[])
         if(!app.isRunning())
         {
             app.setApplicationName(QObject::tr("PhotoFiltre LX Studio"));
-            app.setApplicationVersion(QObject::tr("1.0 Beta (Build 92)"));
+            app.setApplicationVersion(QObject::tr("1.0 Beta (Build 93)"));
             app.setOrganizationDomain(QObject::tr("photofiltre-lx.org"));
             app.setOrganizationName(QObject::tr("photofiltre-lx"));
 
