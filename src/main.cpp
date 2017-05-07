c30158cc69b98834bb3c2b41ba0ea162df029a09
DylanC <dylan_coakley@yahoo.co.uk>
2017-05-07 20:24:30 +0100

    Add new Eraser Icon

--- src/main.cpp
+++ src/main.cpp
@@ -24,7 +24,7 @@ int main(int argc, char *argv[])
         if(!app.isRunning())
         {
             app.setApplicationName(QObject::tr("PhotoFiltre LX"));
-            app.setApplicationVersion(QObject::tr("1.0.6"));
+            app.setApplicationVersion(QObject::tr("1.0.7"));
             app.setOrganizationDomain(QObject::tr("photofiltre-lx.org"));
             app.setOrganizationName(QObject::tr("photofiltre-lx"));
 
