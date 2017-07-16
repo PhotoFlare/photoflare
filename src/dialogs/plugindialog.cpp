3fe35b234345cae4266025a43b6bbabcf7186a1e
DylanC <dylan_coakley@yahoo.co.uk>
2017-07-16 15:22:37 +0000

    Add the PluginDialog with basic plugin listing

--- src/dialogs/plugindialog.cpp
+++ src/dialogs/plugindialog.cpp
@@ -1,11 +1,31 @@
 #include "plugindialog.h"
 #include "ui_plugindialog.h"
 
+#include <QDebug>
+#include <QDir>
+
 PluginDialog::PluginDialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::PluginDialog)
 {
     ui->setupUi(this);
+
+    QDir dir("plugins");
+    if (!dir.exists())
+    {
+        qWarning("Cannot find the plugins directory");
+    }
+    else
+    {
+        ui->listWidget->addItems(dir.entryList(QDir::NoDotAndDotDot|QDir::AllEntries));
+    }
+
+    int count = ui->listWidget->count();
+    for(int i = 0; i < count; i++)
+    {
+      QListWidgetItem *item = ui->listWidget->item(i);
+      item->setSizeHint(QSize(item->sizeHint().width(), 50));
+    }
 }
 
 PluginDialog::~PluginDialog()
