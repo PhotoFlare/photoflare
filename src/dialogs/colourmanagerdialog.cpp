cd00db6284c98b4f614a950486f810c2adc34fb5
DylanC <dylan_coakley@yahoo.co.uk>
2017-10-17 22:39:25 +0000

    Fix size for QLabel to display

--- src/dialogs/colourmanagerdialog.cpp
+++ src/dialogs/colourmanagerdialog.cpp
@@ -1,3 +1,15 @@
+/*
+
+  Dialog for managing colour palettes
+
+*/
+
+//#include <QDebug>
+
+#include <QBoxLayout>
+#include <QLabel>
+#include <QWidget>
+
 #include "colourmanagerdialog.h"
 #include "ui_colourmanagerdialog.h"
 
@@ -15,7 +27,25 @@ colourManagerDialog::~colourManagerDialog()
 
 void colourManagerDialog::on_addButton_clicked()
 {
+    QLabel *label = new QLabel();
+    label->setAutoFillBackground(true);
 
+    QPalette palette = label->palette();
+    palette.setColor(label->backgroundRole(), Qt::black);
+    palette.setColor(label->foregroundRole(), Qt::yellow);
+    label->setPalette(palette);
+
+    QListWidgetItem *item = new QListWidgetItem();
+    item->setSizeHint(QSize(0, 30));
+
+    QHBoxLayout *layout = new QHBoxLayout();
+    layout->addWidget(label);
+
+    QWidget *widget = new QWidget();
+    widget->setLayout(layout);
+
+    ui->listWidget->addItem(item);
+    ui->listWidget->setItemWidget(item,widget);
 }
 
 void colourManagerDialog::on_removeButton_clicked()
