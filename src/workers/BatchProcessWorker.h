e5614ff81f418cefd4f34e5db484f608188c5c22
DylanC <dylan_coakley@yahoo.co.uk>
2017-12-30 17:52:24 +0000

    Move all workers to separate workers folder

---
+++ src/workers/BatchProcessWorker.h
@@ -0,0 +1,29 @@
+#ifndef BATCHPROCESSWORKER_H
+#define BATCHPROCESSWORKER_H
+
+#include <QObject>
+
+#include "dialogs/batchdialog.h"
+#include "mainwindow.h"
+
+class BatchProcessWorker : public QObject
+{
+    Q_OBJECT
+public:
+    explicit BatchProcessWorker(QObject *parent = 0);
+
+    void setParams(batchDialog* params){m_params = params;}
+    void setParent(MainWindow* parent){m_parent = parent;}
+public slots:
+    void process();
+
+signals:
+
+    void fileProcessFinished(QString file, QImage image);
+    void batchProgress(int idx, int total);
+private:
+    batchDialog* m_params;
+    MainWindow* m_parent;
+};
+
+#endif // BATCHPROCESSWORKER_H
