cbd4c18d2dc31f230e3e6200a52f5d2f58dab9a5
Dean Coakley <dean.s.coakley@gmail.com>
2018-01-28 20:15:33 +0000

    diffing tool now takes args. now broken

--- languages/diffing_tool.py
+++ languages/diffing_tool.py
@@ -1,20 +1,26 @@
 """This module outputs strings missing from other .ts files"""
+import sys
+
 EN_FILE = open("en.ts", "r")
 FR_FILE = open("fr.ts", "r")
-EN_STRINGS, FR_STRINGS = [], []
+EN_STRINGS, LANG_STRINGS = [], []
 
 for en_string in EN_FILE:
     en_string = en_string.lstrip()
     if en_string.startswith("<source>"):
         EN_STRINGS.append(en_string[8:-10])
 
-for fr_string in FR_FILE:
-    fr_string = fr_string.lstrip()
+for arg in sys.argv[1:]:
+    LANG_FILE = arg + ".ts"
+    LANG_STRINGS = []
 
-    if fr_string.startswith("<source>"):
-        fr_string = fr_string[8:-10]
-        FR_STRINGS.append(fr_string)
+    for lang_string in LANG_FILE:
+        lang_string = lang_string.lstrip()
 
-for string in EN_STRINGS:
-    if string not in FR_STRINGS:
-        print string
+        if lang_string.startswith("<source>"):
+            lang_string = lang_string[8:-10]
+            LANG_STRINGS.append(lang_string)
+
+        for string in EN_STRINGS:
+            if string not in LANG_STRINGS:
+                print (string)
