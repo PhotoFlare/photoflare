f0c620bb46a99584b5664f095d6eb8f6b75afaff
DylanC <dylan_coakley@yahoo.co.uk>
2019-02-13 21:09:57 +0000

    Test HiDPI scaling

--- src/main.cpp
+++ src/main.cpp
@@ -20,6 +20,8 @@
 
 int main(int argc, char *argv[])
 {
+    QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
     QtSingleApplication app("PhotoFlare", argc, argv);
 
     // Remove WhatIsThis from Window Controls
