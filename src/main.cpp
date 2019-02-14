3500dad4d544efde303264d0be8bf54b2dc96cc5
DylanC <dylan_coakley@yahoo.co.uk>
2019-02-14 13:33:51 +0000

    Remove HiDPI testing via setAttribute

--- src/main.cpp
+++ src/main.cpp
@@ -20,8 +20,6 @@
 
 int main(int argc, char *argv[])
 {
-    QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-
     QtSingleApplication app("PhotoFlare", argc, argv);
 
     // Remove WhatIsThis from Window Controls
