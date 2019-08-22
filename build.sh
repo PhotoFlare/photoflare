02080e62d2afa32e9c43a18cc642cec0f13d90bc
DylanC <dylan_coakley@yahoo.co.uk>
2019-08-22 15:38:40 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@
 export QT_SELECT=qt5
 
 apt-get update -q
-apt-get install -y qtbase5-dev qttools5-dev-tools libgraphicsmagick++-dev
+apt-get install -y qtbase5-dev qttools5-dev-tools libgraphicsmagick++-dev libomp-dev
 
 qmake PhotoFlare.pro
 make
