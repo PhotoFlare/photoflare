9aa0cebc4872d589bdf0b9783d1741bce537fb84
DylanC <dylan_coakley@yahoo.co.uk>
2019-09-26 12:07:25 +0100

    Update build script for .pro file rename

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ export QT_SELECT=qt5
 apt-get update -q
 apt-get install -y qtbase5-dev qttools5-dev-tools libgraphicsmagick++-dev libomp-dev
 
-qmake PhotoFlare.pro
+qmake Photoflare.pro
 make
 # make install # Before to enable it, PhotoFlare.pro need to be adjusted
 make clean
