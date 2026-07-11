5441e317bb6eef5cf0ec9c73992da044d81e2fcd
Dylan Coakley <dylan_coakley@yahoo.co.uk>
2026-07-11 18:00:42 +0100

    Prepare v1.7.2 release

--- src/main.cpp
+++ src/main.cpp
@@ -39,7 +39,7 @@ int main(int argc, char *argv[])
     {
         // App details
         app.setApplicationName("photoflare");
-        app.setApplicationVersion("1.7.1");
+        app.setApplicationVersion("1.7.2");
         app.setOrganizationDomain("photoflare.io");
 
         // Setup Default settings
